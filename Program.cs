2064de0c07b6a5a96aed08c6ec35a8ea4c0f47c9
nikito1993 <120242077+nikito1993@users.noreply.github.com>
2023-03-01 21:18:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿ namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> Kids = new Queue<string>(Console.ReadLine().Split(" ").ToList());
+
+            int tosses=int.Parse(Console.ReadLine());
+            int toss = 1;
+            while (Kids.Count>1)
+            {
+                string currKid=Kids.Dequeue();
+                if (toss==tosses)
+                {
+                    Console.WriteLine($"Removed {currKid}");
+                    toss=1;
+                    continue;
+                }
+                toss++;  
+                Kids.Enqueue(currKid);
+            }
+            Console.WriteLine($"Last is {Kids.Dequeue()}");
+        }
+    }
+}
